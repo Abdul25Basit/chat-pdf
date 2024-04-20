8c64eb13df23a1535131f56de5e6033ba7ada094
Abdul25Basit <136110055+Abdul25Basit@users.noreply.github.com>
2024-04-20 21:57:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import gradio as gr
+import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.llms import HuggingFaceHub
 from langchain_community.embeddings import HuggingFaceEmbeddings
@@ -7,50 +7,39 @@ from langchain.chains import RetrievalQA
 from langchain_community.document_loaders import PyMuPDFLoader
 
 
-import os
+import streamlit as st
 
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 
 def load_doc(pdf_doc):
 
-    loader = PyMuPDFLoader(pdf_doc.name)
-    documents = loader.load()
-    embedding = HuggingFaceEmbeddings()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    text = text_splitter.split_documents(documents)
-    db = Chroma.from_documents(text, embedding)
-    llm = HuggingFaceHub(repo_id="OpenAssistant/oasst-sft-1-pythia-12b", model_kwargs={"temperature": 1.0, "max_length": 256})
-    global chain
-    chain = RetrievalQA.from_chain_type(llm=llm,chain_type="stuff",retriever=db.as_retriever())
-    return 'Document has successfully been loaded'
+  loader = PyMuPDFLoader(pdf_doc)
+  documents = loader.load()
+  embedding = HuggingFaceEmbeddings()
+  text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+  text = text_splitter.split_documents(documents)
+  db = Chroma.from_documents(text, embedding)
+  llm = HuggingFaceHub(repo_id="OpenAssistant/oasst-sft-1-pythia-12b", model_kwargs={"temperature": 1.0, "max_length": 256})
+  global chain
+  chain = RetrievalQA.from_chain_type(llm=llm,chain_type="stuff",retriever=db.as_retriever())
+  return 'Document has successfully been loaded'
 
 def answer_query(query):
-    question = query
-    return chain.run(question)
-html = """
-<div style="text-align:center; max width: 700px;">
-    <h1>ChatPDF</h1>
-    <p> Upload a PDF File, then click on Load PDF File <br>
-    Once the document has been loaded you can begin chatting with the PDF =)
-</div>"""
-css = """container{max-width:700px; margin-left:auto; margin-right:auto,padding:20px}"""
-with gr.Blocks(css=css,theme=gr.themes.Monochrome()) as demo:
-    gr.HTML(html)
-    with gr.Column():
-        gr.Markdown('ChatPDF')
-        pdf_doc = gr.File(label="Load a pdf",file_types=['.pdf','.docx'],type='filepath')
-        with gr.Row():
-            load_pdf = gr.Button('Load pdf file')
-            status = gr.Textbox(label="Status",placeholder='',interactive=False)
+  question = query
+  return chain.run(question)
 
+st.title("ChatPDF")
+st.write("Upload a PDF File and then click on Load PDF File")
+st.write("Once the document has been loaded you can begin chatting with the PDF =)")
 
-        with gr.Row():
-            input = gr.Textbox(label="type in your question")
-            output = gr.Textbox(label="output")
-        submit_query = gr.Button("submit")
+uploaded_pdf = st.file_uploader("Load a pdf", type=['.pdf','.docx'])
 
-        load_pdf.click(load_doc,inputs=pdf_doc,outputs=status)
+if uploaded_pdf is not None:
+  status = st.text_input("Status", "", key="status")
+  status.value = load_doc(uploaded_pdf)
 
-        submit_query.click(answer_query,input,output)
+  query = st.text_input("type in your question")
+  output = st.text_area("output", "", height=100)
 
-demo.launch()
+  if st.button("submit"):
+    output.value = answer_query(query)
