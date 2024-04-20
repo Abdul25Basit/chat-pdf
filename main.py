3a5d2d7995b71102209bffd313a8bba23f602b4b
Abdul25Basit <136110055+Abdul25Basit@users.noreply.github.com>
2024-04-20 21:51:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import gradio as gr
+
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.llms import HuggingFaceHub
 from langchain_community.embeddings import HuggingFaceEmbeddings
